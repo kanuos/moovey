277c9525a1d205e2a9acdba6d34363ff5706ca19
kanuos <sounakmukherjee@ymail.com>
2021-04-22 15:52:14 -0400

    setup : redis client, helmet. imgBB,

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-// SERVER SETTINGS
+// environment variables
 require("dotenv").config();
+const {
+    REDIS_URL,
+    SESSION_NAME,
+    SESSION_SECRET,
+    PORT
+} = process.env
+
+// SERVER SETTINGS
 const express = require('express');
 const session = require("express-session");
+const redis = require("redis");
+const redisClient = redis.createClient(REDIS_URL)
+const RedisStore = require("connect-redis")(session)
 const path = require('path');
 const app = express();
 const favicon = require("serve-favicon");
+const fileUpload = require("express-fileupload");
+const helmet = require("helmet");
 
 // SERVER CONFIGURATION
 
+app.use(helmet({
+    contentSecurityPolicy : {
+        directives : {
+            defaultSrc : ["*"],
+            imgSrc : ["http:","https:", "blob:"]
+        }
+    }
+}));
 app.set("view engine", "ejs");
 app.set('views', './View');
 app.use('/static',express.static(path.join(__dirname, 'static')));
@@ -15,27 +36,36 @@ app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(favicon(path.join(__dirname, "static", "assets", "favicon.png")))
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/TEMP_FILES'
+}))
+
 
 
 // MIDDLEWARES
 require("./_Database")  // database connection
 app.use(session({
-    name : process.env.SESSION_NAME,
-    secret : process.env.SESSION_SECRET,
+    name : SESSION_NAME,
+    secret : SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie : {
         httpOnly: true,
-        maxAge : 1000 * 60 * 5,
+        maxAge : 1000 * 60 * 15,
         sameSite : true,
-    }
+    },
     // store
+    store : new RedisStore({
+        client: redisClient,
+        url : REDIS_URL ?? null
+    })
 }))
 
 // ROUTE MIDDLEWARES
 app.use('/', require('./_Routes/accountRoutes'))
-app.use('/blogs', require('./_Routes/blogRoutes'))
+app.use('/moovey', require('./_Routes/blogRoutes'))
 
 app.get('*', (req, res) => res.render("pages/404", {title : 'Page Not Found'}))
 
-app.listen(8000, ()=> console.log("Server running on port 8000"));
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
