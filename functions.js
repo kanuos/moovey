f3353c102f4103aec3a9ffae93629c0d78d55d8e
kanuos <sounakmukherjee@ymail.com>
2021-06-17 15:25:29 -0400

    updated util functions

--- functions.js
+++ functions.js
@@ -1,30 +1,6 @@
-const axios = require('axios');
-
-const URL = `http://www.omdbapi.com/?apikey=${process.env.MOVIE_API_KEY}&`
-
 const MONTHS = [
     "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
 ]
-
-const getMovieTitles = async (searchKeyword) => {
-    try {
-        const response = await axios(URL + `s=${searchKeyword}`);
-        return response.data.Search;
-    } catch (error) {
-        return [];
-    }
-}
-
-const getFullImage = imageURL => {
-    /*
-    converts a 300 * 300 thumbnail to full poster
-    Sample URL
-    https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg
-    */
-   if (imageURL.includes("._V1_SX300"))
-        return imageURL.replace("._V1_SX300")
-    return imageURL;
-}
 
 const minimumLength = (input="", minLength = 6) => {
     return input.length >= minLength
@@ -101,7 +77,6 @@ const formatAPIResponse = obj => {
 
 module.exports = {
     formatAPIResponse,
-    getMovieTitles, 
     slugify,
     getFullImage,
     minimumLength,
